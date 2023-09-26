8747b0c997f3cb01b131b3c9f776667642a98cb8
VolovikovMikhail <volovikov.1995@bk.ru>
2023-09-26 21:58:16 +0500

    Task5

---
+++ Tyuiu.VolovikovMV.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VolovikovMV.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int time = 38542;
+            var res = ds.SecondsToHours(time);
+            Assert.AreEqual(10, res);
+        }
+    }
+}
---
+++ Tyuiu.VolovikovMV.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint1.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Воловиков М. В. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                             *");
+            Console.WriteLine("* Выполнил: Воловиков Михаил Васильевич | АСОиУб-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие: Написать программу, которая запрашивает у пользователя         *");
+            Console.WriteLine("* исходные данные, выполняет указанные расчёты и                          *");
+            Console.WriteLine("* печатает результат на экране.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int time;
+            Console.WriteLine("Введите количество секунд:");
+            time = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество полных часов = " + ds.SecondsToHours(time));
+
+            Console.ReadKey();
+        }
+    }
+}
